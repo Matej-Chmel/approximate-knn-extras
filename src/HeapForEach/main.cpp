fdc2ae375e30622ec554d6fb943aa6b1c6efa26c
Matej-Chmel <58189701+Matej-Chmel@users.noreply.github.com>
2022-05-12 16:11:24 +0200

    HeapForEach simulates efConstruction = 200.

--- src/HeapForEach/main.cpp
+++ src/HeapForEach/main.cpp
@@ -6,12 +6,12 @@ int main() {
 	using namespace chm;
 
 	return catchAllExceptions([]() {
-		HeapForEachArgs args(generateNodes(100, 0.f, 1000000.f, 30));
+		HeapForEachArgs args(generateNodes(200, 0.f, 1000000.f, 30));
 
 		BenchmarkSuite<HeapForEachArgs>(args, "Benchmark of heap iteration methods", args.getCorrectRes())
 			.add(iterateQueue, "iterate queue", true)
 			.add(iterateVector, "iterate vector", false)
-			.repeat(500000)
+			.repeat(300000)
 			.print(std::cout, "\n\n");
 	});
 }
